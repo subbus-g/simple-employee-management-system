6a01c8151fc7b097a4af5fedfff7a61a00266435
subbus <subbareddyg83@gmail.com>
2020-11-23 10:08:51 +0530

    add welcome message

---
+++ src/main/java/tech/subbus/crud_operations_using_spring_mvc/CrudOperationsUsingSpringMvcApplication.java
@@ -0,0 +1,15 @@
+package tech.subbus.crud_operations_using_spring_mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudOperationsUsingSpringMvcApplication 
+{
+
+	public static void main(String[] args) 
+	{
+		SpringApplication.run(CrudOperationsUsingSpringMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/subbus/crud_operations_using_spring_mvc/CrudOperationsUsingSpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.subbus.crud_operations_using_spring_mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudOperationsUsingSpringMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
