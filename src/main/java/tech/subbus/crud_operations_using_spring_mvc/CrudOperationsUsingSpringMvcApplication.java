da64600de0a34deec33ce0980ce4857c04ce4005
subbus <subbareddyg83@gmail.com>
2020-11-23 15:21:49 +0530

    format the code

--- src/main/java/tech/subbus/crud_operations_using_spring_mvc/CrudOperationsUsingSpringMvcApplication.java
+++ src/main/java/tech/subbus/crud_operations_using_spring_mvc/CrudOperationsUsingSpringMvcApplication.java
@@ -4,11 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CrudOperationsUsingSpringMvcApplication 
-{
+public class CrudOperationsUsingSpringMvcApplication {
 
-	public static void main(String[] args) 
-	{
+	public static void main(String[] args) {
 		SpringApplication.run(CrudOperationsUsingSpringMvcApplication.class, args);
 	}
 
--- src/main/java/tech/subbus/crud_operations_using_spring_mvc/entity/Employee.java
+++ src/main/java/tech/subbus/crud_operations_using_spring_mvc/entity/Employee.java
@@ -8,88 +8,75 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="employee")
-public class Employee 
-{
+@Table(name = "employee")
+public class Employee {
 
-	// define fields
-	
-	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	@Column(name="id")
-	private int id;
-	
-	@Column(name="first_name")
-	private String firstName;
-	
-	@Column(name="last_name")
-	private String lastName;
-	
-	@Column(name="email")
-	private String email;
-	
-		
-	// define constructors
-	
-    public Employee() 
-    {
-		
-	}
+    // define fields
 
-    public Employee(String firstName, String lastName, String email) 
-    {
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.email = email;
-	}
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
 
+    @Column(name = "first_name")
+    private String firstName;
 
-	// define getter/setter
-    public int getId() 
-    {
-		return id;
-	}
+    @Column(name = "last_name")
+    private String lastName;
 
-    public void setId(int id) 
-    {
-		this.id = id;
-	}
+    @Column(name = "email")
+    private String email;
 
-    public String getFirstName() 
-    {
-		return firstName;
-	}
+    // define constructors
 
-    public void setFirstName(String firstName) 
-    {
-		this.firstName = firstName;
-	}
+    public Employee() {
 
-    public String getLastName() 
-    {
-		return lastName;
-	}
+    }
 
-    public void setLastName(String lastName) 
-    {
-		this.lastName = lastName;
-	}
+    public Employee(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
 
-    public String getEmail() 
-    {
-		return email;
-	}
+    // define getter/setter
+    public int getId() {
+        return id;
+    }
 
-    public void setEmail(String email) 
-    {
-		this.email = email;
-	}
+    public void setId(int id) {
+        this.id = id;
+    }
 
-	// define tostring
+    public String getFirstName() {
+        return firstName;
+    }
 
-	@Override
-	public String toString() {
-		return "Employee [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + "]";
-	}
-		
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    // define tostring
+
+    @Override
+    public String toString() {
+        return "Employee [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + "]";
+    }
+
 }
